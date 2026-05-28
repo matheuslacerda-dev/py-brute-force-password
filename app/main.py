44698803718eeb4e3d4c2ee9c4b023cf067873df
matheuslacerda-dev <matheuslacerda01@proton.me>
2026-05-28 19:01:31 -0300

    refactoring imports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import asyncio
-import hashlib
 import time
 from concurrent.futures import ProcessPoolExecutor
+from hashlib import sha256
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -18,14 +18,14 @@ PASSWORDS_TO_BRUTE_FORCE = [
 
 
 def sha256_hash_str(to_hash: str) -> str:
-    return hashlib.sha256(to_hash.encode("utf-8")).hexdigest()
+    return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
 def brute_force_password(start: int, end: int, target_hash: set) -> list:
     results = []
     for num in range(start, end):
         password = f"{num:08d}"
-        generated_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+        generated_hash = sha256_hash_str(password)
         if generated_hash in target_hash:
             results.append((password, generated_hash))
 
